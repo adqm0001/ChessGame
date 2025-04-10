00a2dea64c04579aa51d3cdffeae441df36123eb
adqm0001 <adam.benmoussa04@gmail.com>
2025-04-10 01:06:42 -0400

    Add JFrame

--- Board.java
+++ Board.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+public class Board extends JPanel {
 
-public class Board extends JFrame {
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 800;
+
     public Board(){
-
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
+    public static void main(String[] args){
+        JFrame jf = new JFrame("Chess");
+        jf.setResizable(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+        jf.setLocationRelativeTo(null);
+
+        Board bd = new Board();
+        jf.add(bd);
+    }
 }
